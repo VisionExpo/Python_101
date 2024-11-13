a3b9b04a7e6ad2d96afd2470864243c31c43d414
Vision Expo <gorulevishal984@gmail.com>
2024-11-13 21:51:23 +0530

    modular coding

---
+++ 5-Moduler_Coding/custom_package/__init__.py
@@ -0,0 +1,4 @@
+'''
+    __init__.py is a special file used to define packages and 
+    initialize their namespaces
+'''
---
+++ 5-Moduler_Coding/test.py
@@ -0,0 +1,6 @@
+from custom_package.maths import *
+from custom_package.sub_package.mult import multiply
+
+print(addition(2,3))
+print(subtraction(3,2))
+print(multiply(2,3))
